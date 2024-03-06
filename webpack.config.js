086200a0b121b0ab34a68663d2c3ff184d84bdcc
rafacristi17 <rafaelacristine@live.com>
2024-03-05 22:44:30 -0300

    Inicio do projeto Linketinder com TSC e Webpack configurado

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fortTsCheckWebpackPlugin=require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new fortTsCheckWebpackPlugin()],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'public', 'dist'),
+  },
+};
