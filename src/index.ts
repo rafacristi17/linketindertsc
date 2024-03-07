1763acd793a435a1115362aedf6d9118f10c6e59
rafacristi17 <rafaelacristine@live.com>
2024-03-06 22:56:41 -0300

    fix: Corrigindo as interfaces  do perfil  Candidato

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,25 @@
-interface Habilidade {
-    habilidade: string;
-    percentual: number;
-}
-
 interface Tecnologia {
-    tecnologia: string;
-    percentual: number;
+   mysql:boolean;
+   java:boolean;
+   node:boolean;
+   typescript:boolean;
+   javascript:boolean;
+   git:boolean;
 }
 
 interface Candidato {
     nome: string;
-    habilidades: Habilidade[];
-    tecnologias: Tecnologia[];
+    email:string;
+    cpf:string;
+    date:string;
+    estado:string;
+    message:string;
+    senha:string;
+    tecnologias:Tecnologia;
 }
 
-interface Empresa {
-    nome: string;
-    habilidades: Habilidade[];
-    tecnologias: Tecnologia[];
-}
+let candidatos: Candidato[] = JSON.parse(localStorage.getItem('candidatos')!) || [];
 
-let candidatos: Candidato[] = JSON.parse(localStorage.getItem('candidatos')!) || [
-    {
-        nome: "Candidato 1",
-        habilidades: [
-            { habilidade: "java", percentual: 80 },
-            { habilidade: "groovy", percentual: 75 },
-        ],
-        tecnologias: [
-            { tecnologia: "typescript", percentual: 85 },
-            { tecnologia: "javascript", percentual: 90 },
-        ]
-    }, {
-        nome: "Candidato 2",
-        habilidades: [
-            { habilidade: "python", percentual: 20 },
-            { habilidade: "mongodb", percentual: 50 },
-        ],
-        tecnologias: [
-            { tecnologia: "git", percentual: 85 },
-            { tecnologia: "lógica de programação", percentual: 90 },
-        ]
-    },
-];
-function saveSkills(): void {
-    let skillInput: HTMLInputElement = <HTMLInputElement>document.getElementById("skillInput");
-    let skillPercent: HTMLInputElement = <HTMLInputElement>document.getElementById("skillPercent");
-    if (skillInput && skillPercent) {
-        candidatos[0].habilidades.push({ habilidade: skillInput.value, percentual: parseInt(skillPercent.value) });
-        localStorage.setItem('candidatos', JSON.stringify(candidatos));
-        displaySkills();
-    }
-}
-function saveTech(): void {
-    let techInput: HTMLInputElement = <HTMLInputElement>document.getElementById("techInput");
-    let techPercent: HTMLInputElement = <HTMLInputElement>document.getElementById("techPercent");
-    if (techInput && techPercent) {
-        candidatos[0].tecnologias.push({ tecnologia: techInput.value, percentual: parseInt(techPercent.value) });
-        localStorage.setItem('candidatos', JSON.stringify(candidatos));
-        displayTech();
-    }
-}
-function displaySkills() {
-    var skillsDiv = document.getElementById("skills")!;
-    skillsDiv.innerHTML = "";
-    for (var i = 0; i < candidatos[0].habilidades.length; i++) {
-        skillsDiv.innerHTML += '<div class="skill"><span class="name">' + candidatos[0].habilidades[i].habilidade + '</span><div class="bar"><div class="progress" style="width: ' + candidatos[0].habilidades[i].percentual + '%;"></div></div></div>';
-    }
-}
-function displayTech() {
-    var techDiv = document.getElementById("tech")!;
-    techDiv.innerHTML = "";
-    for (var i = 0; i < candidatos[0].tecnologias.length; i++) {
-        techDiv.innerHTML += '<div class="skill"><span class="name">' + candidatos[0].tecnologias[i].tecnologia + '</span><div class="bar"><div class="progress" style="width: ' + candidatos[0].tecnologias[i].percentual + '%;"></div></div></div>';
-    }
-}
 function openTab(evt: Event, tabName: string): void {
     let i: number;
     let tabcontent: HTMLCollectionOf<Element>;
